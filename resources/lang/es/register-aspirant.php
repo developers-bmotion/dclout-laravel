ce12190de7dedc573330e26382b5f5b8839c775b
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-11-25 17:51:00 -0500

    Ajustes en el registro

---
+++ resources/lang/es/register-aspirant.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    //REGISTRO DE USUARIOS----------------------------------------------------------------------------------------------
+    'informacion_personal' => 'Información personal',
+    'perfilacion' => "Perfilación",
+    'nombres' => 'Nombre',
+    'apellidos' => 'Apellido',
+    'correo' => 'Correo Electrónico',
+    'selecciona_pais' => 'Selecciona tu pais',
+    'selecciona_ciudad' => 'Selecciona tu ciudad',
+    'telefono' => 'Teléfono',
+    'fecha_nacimiento' => 'Fecha de Nacimiento',
+    'mensaje_creador_contenido' => 'Soy creador de contenido y deseo participar en las oportunidades de proyectos de d.',
+    'btn_registar' => 'Registar',
+    'btn_siguiente' => 'Siguiente',
+    'btn_atras' => 'Átras',
+    'si' => 'Si',
+    'no' => 'No',
+    'selecciona' => 'Selecciona',
+
+
+    //Mensajes de error
+    'error_name' => 'Ingrese un nombre valido',
+    'error_last_name' => 'Ingrese apellido valido',
+    'error_email' => 'Ingrese un email valido',
+    'error_fecha_nacimiento' => 'Ingrese una fecha de nacimiento válida',
+    'error_telefono' => 'Ingrese número de teléfono válido',
+
+    //Mensajes de requerido
+    'requerido_name' => 'El nombre es requerido',
+    'requerido_last_name' => 'El apellido es requerido',
+    'requerido_email' => 'El email es requerido',
+    'requerido_fecha_nacimiento' => 'La fecha de nacimiento es requerida',
+    'requerido_telefono' => 'El teléfono es requerido',
+    'requerido_seleccionar_pais' => 'Debes seleccionar tu pais',
+    'requerido_seleccionar_ciudad' => 'Debes seleccionar tu ciudad',
+
+    //PERFILACIÓN-------------------------------------------------------------------------------------------------------
+    'seleccionar_categoria_profiling' => 'Elige la categoría que más se relacione a ti',
+    'seleccionar_tags_profiling' => 'Elige tus tags más afines',
+    'seleccionar_music_profiling' => 'Elige tus géneros de música más afines',
+    'mensaje_desea_enviar_proyecto' => '¿Tienes un proyecto de emprendimiento para presentar? Selecciona si y envíanos la información',
+
+    //Mensajes de requerido
+    'requerido_selecc_categoria_perfilacion' => 'Debes seleccionar una categoría',
+    'requerido_selecc_tags_perfilacion' => 'Debes seleccionar al menos una etiqueta',
+    'requerido_selecc_music_perfilacion' => 'Debes seleccionar al menos un género musical',
+
+
+    //PRESENTA TU PROYECTO----------------------------------------------------------------------------------------------
+    'presenta_proyecto' => 'Presenta tu proyecto',
+];
